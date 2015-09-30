adc6b3d740a0663c1f7529f002b43f0281de11b1
Ricardo Ibarra <ribarra@tekmexico.com>
2015-09-30 14:33:22 -0700

    canvas prototype page done

--- client/components/toolbar/toolbar.controller.js
+++ client/components/toolbar/toolbar.controller.js
@@ -1,6 +1,11 @@
 angular.module('teamDrawApp').controller('toolbarCtrl', function($scope, $mdSidenav) {
-
+    var originatorEv;
     $scope.sideBar = function() {
-        $mdSidenav('right').toggle();
-    }
+        $mdSidenav('rightSidebar').toggle();
+    };
+
+    $scope.openMenu = function($mdOpenMenu, ev) {
+        originatorEv = ev;
+        $mdOpenMenu(ev);
+    };
 });
